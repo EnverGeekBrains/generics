b130df16f737df30650acca8af4bb4714fe27bf5
EnverGeekBrains <envereskendarov@gmail.com>
2018-10-21 14:46:02 +0300

    Метод преобразовывает массив в ArrayList

--- src/main/java/ru/eskendarov/ea/CastlingOfElements.java
+++ src/main/java/ru/eskendarov/ea/CastlingOfElements.java
@@ -1,9 +1,10 @@
 package ru.eskendarov.ea;
 
 /*
-*   Написать метод, который меняет два элемента массива местами,
-*   массив может быть любого ссылочного типа.
-* */
+ *   Написать метод, который меняет два элемента массива местами,
+ *   массив может быть любого ссылочного типа.
+ *
+ * */
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,7 @@ public class CastlingOfElements {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>(Arrays.asList("Ночь", "Улица",
                 "Фонарь", "Аптека", "Задача", "Книга", "Библиотека"));
+
         List<Integer> integers = new ArrayList<>(Arrays.asList(-244, 566, 35, 0, -200, 446, 324, 2));
 
         System.out.println(strings);
@@ -25,15 +27,15 @@ public class CastlingOfElements {
         System.out.println(integers);
     }
 
-//    Метод меняет два элемента в списке местами
-        private static <E> void toSwap(List<E> list, final int element1, final int element2) {
+    //    Метод меняет два элемента в списке местами
+    private static <E> void toSwap(List<E> list, final int element1, final int element2) {
         try {
             E e1 = list.get(element1);
             E e2 = list.get(element2);
             list.set(element1, e2);
             list.set(element2, e1);
-        } catch (IndexOutOfBoundsException e){
-            System.err.println("Задан индекс за пределами массива: "+e);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Задан индекс за пределами массива: " + e);
         }
     }
 }
